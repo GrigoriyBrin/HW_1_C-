a56e2900632cb5f0bc3eb0a1d5eb62b18693514c
GrigoriyBrin <brinzeigrigorii@gmail.com>
2022-06-25 21:15:55 +0300

    Решение задачи 4.2

---
+++ Task_4.2/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] N = {1, 2, 3, 4, 5, 6, 7, 8};
+int a = N.Length;
+int index = 0;
+
+while (N[index] < a - 1)
+{
+    if (N[index] % 2 == 0)
+    {
+        Console.WriteLine(N[index]);
+    }
+   index++;
+   
+}
+    
