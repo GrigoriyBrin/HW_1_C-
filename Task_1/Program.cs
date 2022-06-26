b947f993ec24d5f55575298cbb67eda87fb05f87
GrigoriyBrin <brinzeigrigorii@gmail.com>
2022-06-26 16:34:41 +0300

    Решение задачи 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите 1-е число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите 2-е число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("1-е число больше 2-го");
+}
+
+else
+{
+    Console.WriteLine("2-е число больше 1-го");
+}
