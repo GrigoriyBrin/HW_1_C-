fe9590f704b1531b8f307eb13cb69e44b90ba04e
GrigoriyBrin <brinzeigrigorii@gmail.com>
2022-06-26 17:19:50 +0300

    Решение задачи 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите произвольное целое число больше 1");
+int a = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+Console.WriteLine();
+
+if(a > 1)
+{
+    while(count < a)
+    {
+        if(count % 2 == 0)
+        {
+            Console.WriteLine(count);
+        }
+        count++;
+    }
+}
