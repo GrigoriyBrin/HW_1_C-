999fcea3e5280f10904312f05e9224ab159fa143
GrigoriyBrin <brinzeigrigorii@gmail.com>
2022-06-26 16:55:01 +0300

    Решение задачи 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите произвольное целое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int b = 2; 
+int c = a % b;
+
+if (c == 0)
+{
+    Console.WriteLine("Да");
+}
+
+else
+{   
+    Console.WriteLine("Нет");
+}   
